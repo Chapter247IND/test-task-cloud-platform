d5f19a04b0a59a25c5107c166644f6236a25dc7c
RishabhBula1712 <rishab.chapter247@gmail.com>
2020-10-28 20:54:10 +0530

    Design modifications

--- src/Components/home.tsx
+++ src/Components/home.tsx
@@ -8,6 +8,12 @@ const Home = () => {
   //State for sattellite number
   const [satteliteNumber, setSatteliteNumber] = useState(10000);
 
+  //State for sattellite detail view
+  const [isDetailView, setisDetailView] = useState(false);
+
+  //State for sattellite detail view
+  const [detailsData, setDetailsData] = useState<any>({});
+
   // fetch data from reducer state
   const homeReducer = useSelector((state: any) => state.homeReducer);
 
@@ -33,27 +39,25 @@ const Home = () => {
     dispatch(satteliteDetailRequest({ satelitteNumber: satteliteNumber }));
   };
 
+  const handleDetailView = (item: any) => {
+    setDetailsData(item);
+    setisDetailView(true);
+  };
+
   return (
     <section className="banner-wrap">
-      <div className="img-block">
-        <img
-          src={"../images/home-background.jpg"}
-          alt=""
-          className="img-responsive"
-        />
-      </div>
-      <div className={"caption-block-content"}>
-        <Container>
+      <Container>
+        <div className={"caption-block-content"}>
+          <h1 className={"main-title"}>Test UI Task</h1>
           <h5>
             New ways to get GP data now available! Up until now, we've used the
             TLE format to ingest data into SGP4. With the growth of the catalog
             soon to exceed the current range of 5-digit catalog numbers, you may
             be wondering how we will handle that.{" "}
           </h5>
-        </Container>
-      </div>
-      <div className="caption-block">
-        <Container>
+        </div>
+        <div className="caption-block">
+          <h6 className={"form-label"}>Enter 5 Dight Satellite Number</h6>
           <div className={"text-center"}>
             <input
               className={"form-control"}
@@ -65,10 +69,14 @@ const Home = () => {
           </div>
           <div className={"text-right mt-2"}>
             <Button
+              className={"btn-theme"}
               onClick={() => handleSatelliteDetails()}
               size={"sm"}
+              variant={"link"}
             >
-              Check Sattelite
+              {homeReducer.isSatteliteRequest
+                ? "Please wait..."
+                : "Check Satellite"}
             </Button>
           </div>
           {satteliteError ? (
@@ -76,23 +84,14 @@ const Home = () => {
               <b>{satteliteError}</b>
             </div>
           ) : null}
-        </Container>
-      </div>
-      <div className="caption-block-left">
-        <Container>
-          <Card className={"height-auto"}>
-            <Card.Header>
-              <Row>
-                <Col md={6}>
-                  <h3 className="your-card">Satellite List</h3>
-                </Col>
-              </Row>
-            </Card.Header>
-            {/* <div className="form-wrap animated fadeIn"> */}
-            <Card.Body className={"overflow-auto"}>
-              <Col md={"12"}>
-                <Table responsive bordered>
-                  <thead>
+        </div>
+        <div className="caption-block-left">
+          <Card className={""}>
+            <Card.Body className={""}>
+              <div className={"card-title"}>Satellite List</div>
+              {!isDetailView ? (
+                <Table responsive>
+                  <thead className={"thead-light"}>
                     <tr>
                       <th>Object Id</th>
                       <th>Object Name</th>
@@ -116,7 +115,14 @@ const Home = () => {
                                 {item.NORAD_CAT_ID ? item.NORAD_CAT_ID : "-"}
                               </td>
                               <td>
-                                <Button size={"sm"}>View</Button>
+                                <Button
+                                  size={"sm"}
+                                  className={"btn-detail-view"}
+                                  onClick={() => handleDetailView(item)}
+                                  variant={"link"}
+                                >
+                                  View Details
+                                </Button>
                               </td>
                             </tr>
                           );
@@ -124,16 +130,132 @@ const Home = () => {
                       )
                     ) : (
                       <tr className="text-center">
-                        <div>No data found</div>
+                        <td colSpan={4}>
+                          <h5 className={"text-center"}>No data found</h5>
+                        </td>
                       </tr>
                     )}
                   </tbody>
                 </Table>
-              </Col>
+              ) : (
+                <div>
+                  <div
+                    onClick={() => setisDetailView(false)}
+                    className={"cursor-pointer"}
+                  >
+                    Back to list
+                  </div>
+
+                  <div className="detail-div">
+                    <div className="item-list">
+                      <span className="text-label">Object Id:</span>
+                      <span className="text-value">
+                        {detailsData.OBJECT_ID}
+                      </span>
+                    </div>
+                    <div className="item-list">
+                      <span className="text-label">Object Name:</span>
+                      <span className="text-value">
+                        {detailsData.OBJECT_NAME}
+                      </span>
+                    </div>
+                    <div className="item-list">
+                      <span className="text-label">Arg Of Pericenter:</span>
+                      <span className="text-value">
+                        {detailsData.ARG_OF_PERICENTER}
+                      </span>
+                    </div>
+                    <div className="item-list">
+                      <span className="text-label">BStar:</span>
+                      <span className="text-value">{detailsData.BSTAR}</span>
+                    </div>
+                    <div className="item-list">
+                      <span className="text-label">Classification Type:</span>
+                      <span className="text-value">
+                        {detailsData.CLASSIFICATION_TYPE}
+                      </span>
+                    </div>{" "}
+                    <div className="item-list">
+                      <span className="text-label">Eccentricity:</span>
+                      <span className="text-value">
+                        {detailsData.ECCENTRICITY}
+                      </span>
+                    </div>{" "}
+                    <div className="item-list">
+                      <span className="text-label">Eccentricity</span>
+                      <span className="text-value">
+                        {detailsData.ECCENTRICITY}
+                      </span>
+                    </div>{" "}
+                    <div className="item-list">
+                      <span className="text-label">Element Set No:</span>
+                      <span className="text-value">
+                        {detailsData.ELEMENT_SET_NO}
+                      </span>
+                    </div>{" "}
+                    <div className="item-list">
+                      <span className="text-label">Epoch:</span>
+                      <span className="text-value">{detailsData.EPOCH}</span>
+                    </div>
+                    <div className="item-list">
+                      <span className="text-label">Epoch:</span>
+                      <span className="text-value">{detailsData.EPOCH}</span>
+                    </div>
+                    <div className="item-list">
+                      <span className="text-label">Inclimanation:</span>
+                      <span className="text-value">
+                        {detailsData.INCLINATION}
+                      </span>
+                    </div>
+                    <div className="item-list">
+                      <span className="text-label">Mean Anomaly:</span>
+                      <span className="text-value">
+                        {detailsData.MEAN_ANOMALY}
+                      </span>
+                    </div>
+                    <div className="item-list">
+                      <span className="text-label">Mean Motion:</span>
+                      <span className="text-value">
+                        {detailsData.MEAN_MOTION}
+                      </span>
+                    </div>
+                    <div className="item-list">
+                      <span className="text-label">Mean Motion Ddot:</span>
+                      <span className="text-value">
+                        {detailsData.MEAN_MOTION_DDOT}
+                      </span>
+                    </div>
+                    <div className="item-list">
+                      <span className="text-label">Mean Motion Dot:</span>
+                      <span className="text-value">
+                        {detailsData.MEAN_MOTION_DOT}
+                      </span>
+                    </div>
+                    <div className="item-list">
+                      <span className="text-label">Norad Cat Id:</span>
+                      <span className="text-value">
+                        {detailsData.NORAD_CAT_ID}
+                      </span>
+                    </div>
+                    <div className="item-list">
+                      <span className="text-label">Ra Of Asc Node:</span>
+                      <span className="text-value">
+                        {detailsData.RA_OF_ASC_NODE}
+                      </span>
+                    </div>
+                    <div className="item-list">
+                      <span className="text-label">Rev At Epoch:</span>
+                      <span className="text-value">
+                        {detailsData.REV_AT_EPOCH}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              )}
             </Card.Body>
           </Card>
-        </Container>
-      </div>
+        </div>
+      </Container>
     </section>
   );
 };
